ad058a24931d0de18dd5a91a38b7fa288db3a4c3
tom1484 <angelus0918866416@gmail.com>
2022-11-13 01:19:32 +0800

    Modify basic practice to have command interaction

---
+++ socket/assignments/src/client.c
@@ -0,0 +1,124 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdbool.h>
+
+int main(int argc, char *argv[]) {
+    // Parse target ip address and port from arguments.
+    if (argc < 3) {
+        printf("invalid arguments [<ip> <port>]\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Creact socket.
+    int socket_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (socket_fd == -1) {
+        perror("cannot create socket");
+        exit(EXIT_FAILURE);
+    }
+
+    struct sockaddr_in st_sockaddr;
+    size_t addr_len = sizeof(struct sockaddr_in);
+    memset(&st_sockaddr, 0, sizeof(struct sockaddr_in));
+
+    // Configure socket info..
+    char *ip_address = argv[1];
+    st_sockaddr.sin_family = PF_INET;
+    st_sockaddr.sin_port = htons(atoi(argv[2]));
+    int res = inet_pton(PF_INET, ip_address, &st_sockaddr.sin_addr);
+
+    if (res < 0) {
+        perror("error: first parameter is not a valid address family");
+        close(socket_fd);
+        exit(EXIT_FAILURE);
+    }
+    else if (0 == res) {
+        perror("char string (second parameter does not contain valid ipaddress");
+        close(socket_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    // Send connection request.
+    if (connect(socket_fd, (struct sockaddr *)&st_sockaddr, addr_len) < 0) {
+        perror("error connect failed");
+        close(socket_fd);
+        exit(EXIT_FAILURE);
+    }
+    printf("connected\n");
+
+    int count = 0;
+    char command[32];
+    int com_num;
+
+    char msg[1024];
+    char buf[1024];
+    size_t read_len;
+    while (1) {
+        if ((read_len = recv(socket_fd, buf, sizeof(buf), 0)) < 0) {
+            perror("error read failed");
+            close(socket_fd);
+            exit(EXIT_FAILURE);
+        }
+        else if (read_len == 0) {
+            printf("disconnected\n");
+            break;
+        }
+        else {
+            buf[read_len] = '\0';
+            /* printf("%s\n", buf); */
+
+            char *ptr = strtok(buf, " ");
+            strcpy(command, ptr);
+
+            ptr = strtok(NULL, " ");
+            if (ptr == NULL) {
+                sprintf(msg, "invalid command");
+            }
+            else {
+                char *end;
+                com_num = strtol(ptr, &end, 10);
+                if (end == ptr || errno != 0) {
+                    sprintf(msg, "invalid command");
+                }
+                else {
+                    bool com_exist = 1;
+                    if (strcmp(command, "add") == 0) {
+                        count += com_num;
+                    }
+                    else if (strcmp(command, "sub") == 0) {
+                        count -= com_num;
+                    }
+                    else if (strcmp(command, "mul") == 0) {
+                        count *= com_num;
+                    }
+                    else if (strcmp(command, "div") == 0) {
+                        count /= com_num;
+                    }
+                    else {
+                        com_exist = false;
+                        sprintf(msg, "invalid command");
+                    }
+
+                    if (com_exist) {
+                        sprintf(msg, "%d", count);
+                        printf("count: %d\n", count);
+                    }
+                }
+            }
+
+            send(socket_fd, msg, strlen(msg), 0);
+        }
+    }
+
+    shutdown(socket_fd, SHUT_RDWR);
+    close(socket_fd);
+
+    return 0;
+}
---
+++ socket/assignments/src/server.c
@@ -0,0 +1,103 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+
+int main(int argc, char *argv[]) {
+
+    // Create socket.
+    int server_fd;
+    if ((server_fd = socket(PF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("can not create socket");
+        exit(EXIT_FAILURE);
+    }
+    printf("socket opened\n");
+
+    struct sockaddr_in server_addr;
+    socklen_t addr_len = sizeof(struct sockaddr_in);
+    memset(&server_addr, 0, addr_len);
+
+    // Configure server socket info.
+    int port = atoi(argv[1]);
+    server_addr.sin_family = PF_INET;
+    server_addr.sin_port = htons(port);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+
+    // Bind socket address and file descriptor so that incoming
+    // connections to the address are associated with the descriptor.
+    if (bind(server_fd, (const struct sockaddr *)&server_addr, addr_len)) {
+        perror("error bind failed");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    // Start listen to socket with maximum queue size N.
+    if (listen(server_fd, 10) == -1) {
+        perror("error listen failed");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    char command[32];
+    int com_num;
+    /* fd_set fdset; */
+    char buf[1024];
+    int read_len;
+    char msg[1024];
+    int send_len;
+
+    int client_fd = 0;
+    struct sockaddr_in client_addr;
+    socklen_t client_len = sizeof(client_addr);
+    while (1) {
+        if (client_fd == 0) {
+            if ((client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_len)) < 0) {
+                perror("accept");
+                exit(EXIT_FAILURE);
+            }
+            printf("new connection from %s:%d\n", 
+                    inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+        }
+        else {
+            printf("command: ");
+
+            scanf("%s", command);
+            if (strcmp(command, "q") == 0) {
+                printf("quit\n");
+
+                close(client_fd);
+                client_fd = 0;
+                continue;
+            }
+            scanf("%d", &com_num);
+            sprintf(msg, "%s %d", command, com_num);
+
+            send(client_fd, msg, strlen(msg), 0);
+
+            if ((read_len = recv(client_fd, buf, 1024, 0)) < 0) {
+                perror("error read failed");
+                close(client_fd);
+                exit(EXIT_FAILURE);
+            }
+            else if (read_len == 0) {
+                getpeername(client_fd, (struct sockaddr *)&client_addr, &client_len);
+                printf("host %s:%d disconnectd\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+
+                close(client_fd);
+                client_fd = 0;
+            }
+            else {
+                buf[read_len] = '\0';
+                printf("reply: %s\n", buf);
+            }
+        }
+    }
+
+    return 0;
+}
